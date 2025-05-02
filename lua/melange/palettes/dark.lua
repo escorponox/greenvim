1930c776a044b7f753f341d415e01338940830c6
Carlos Coves Prieto <escorponox@gmail.com>
2025-05-02 12:09:10 +0200

    lalala

--- lua/melange/palettes/dark.lua
+++ lua/melange/palettes/dark.lua
@@ -4,32 +4,66 @@ return {
     bg      = "#0A1008", -- Very dark green-black
     float   = "#102013", -- Slightly lighter dark green
     sel     = "#1A3018", -- Dark green selection
-    ui      = "#4A6B4A", -- Muted green UI elements
+    ui      = "#6ccba0", -- Muted green UI elements
     com     = "#5A9662", -- Medium green for comments
     fg      = "#00FF00", -- Bright neon green - classic Matrix
   },
   b = {
-    red     = "#00FF66", -- Brighter green for operators (=)
-    yellow  = "#6CFF6C", -- Light green instead of yellow
-    green   = "#00FF00", -- Matrix neon green
-    cyan    = "#52FF9A", -- Green-cyan
-    blue    = "#26C95B", -- Green-blue
-    magenta = "#8AE234", -- Lime green instead of magenta
+    red     = "#ba4571", -- = ...  => <>
+    yellow  = "#aaffaa", -- functions, ts types basic, functions, clases, react components, ?
+    green   = "#00FF00", -- export import from
+    cyan    = "#52FF9A", -- component props
+    blue    = "#3aa5ff", -- strings
+    magenta = "#3aa5ff", -- numbers, booleans
   },
   c = {
-    red     = "#0FD75F", -- Medium bright green
-    yellow  = "#25A035", -- Dark green
-    green   = "#78FF78", -- Lighter green
-    cyan    = "#54B378", -- Muted green-cyan
-    blue    = "#3D9A57", -- Dark muted green
-    magenta = "#72C890", -- Pale green
+    red     = "#0FD75F", -- ; & , | . ...
+    yellow  = "#25A035", -- type, const, return keywords
+    green   = "#78FF78", -- React, gitsigns
+    cyan    = "#54B378", -- Custom ts types
+    blue    = "#3D9A57", -- ????
+    magenta = "#3aa5ff", -- null, undefined
   },
   d = {
-    red     = "#0B3315", -- Very dark green
-    yellow  = "#2AC038", -- Brighter green for delimiters ({}())
+    red     = "#0B3315", -- ???
+    yellow  = "#2AC038", -- Brighter green for delimiters ({}()[])
     green   = "#041A0C", -- Nearly black green
     cyan    = "#0D2D1A", -- Very dark green-cyan
     blue    = "#0F2316", -- Very dark blue-green
     magenta = "#1E3923", -- Dark emerald green
   },
 }
+
+-- Cobalt Neon palette
+-- Ansi 0
+-- #141F30
+-- Ansi 1
+-- #FF2322
+-- Ansi 2
+-- #3AA5FF
+-- Ansi 3
+-- #E8E75C
+-- Ansi 4
+-- #8FF6FF
+-- Ansi 5
+-- #781A9F
+-- nsi 6
+-- #8FF6FF
+-- Ansi 7
+-- #BA4571
+-- Ansi 8
+-- #FFF689
+-- Ansi 9
+-- #D42F2F
+-- Ansi 10
+-- #8FF6FF
+-- Ansi 11
+-- #E9F06D
+-- Ansi 12
+-- #3DAADE
+-- Ansi 13
+-- #823030
+-- Ansi 14
+-- #6CCBA0
+-- Ansi 15
+-- #8FF6FF
